6d93c79ca40b79bfe719e6e5cd229d69c70a50ad
zhangsheng <zhsheng26@gmail.com>
2018-01-01 10:29:12 +0800

    init

---
+++ src/main/kotlin/cn/eusunpower/EuSunPowerApplication.kt
@@ -0,0 +1,11 @@
+package cn.eusunpower
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EuSunPowerApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(EuSunPowerApplication::class.java, *args)
+}
